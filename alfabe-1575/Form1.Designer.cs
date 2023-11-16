155e9300175ef271a54836fbd8060f56b5cffed2
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2023-11-17 01:01:52 +0300

    Add files via upload

---
+++ alfabe-1575/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace alfabe_1575
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cbAlfabe = new ComboBox();
+            btnKontrol = new Button();
+            lblSonuc = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Half Seçiniz :";
+            // 
+            // cbAlfabe
+            // 
+            cbAlfabe.FormattingEnabled = true;
+            cbAlfabe.Items.AddRange(new object[] { "A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z" });
+            cbAlfabe.Location = new Point(126, 29);
+            cbAlfabe.Name = "cbAlfabe";
+            cbAlfabe.Size = new Size(151, 28);
+            cbAlfabe.TabIndex = 1;
+            // 
+            // btnKontrol
+            // 
+            btnKontrol.BackColor = Color.DarkCyan;
+            btnKontrol.Location = new Point(125, 72);
+            btnKontrol.Name = "btnKontrol";
+            btnKontrol.Size = new Size(152, 36);
+            btnKontrol.TabIndex = 2;
+            btnKontrol.Text = "KONTROL ET ";
+            btnKontrol.UseVisualStyleBackColor = false;
+            btnKontrol.Click += btnKontrol_Click;
+            // 
+            // lblSonuc
+            // 
+            lblSonuc.AutoSize = true;
+            lblSonuc.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSonuc.Location = new Point(14, 127);
+            lblSonuc.Name = "lblSonuc";
+            lblSonuc.Size = new Size(66, 23);
+            lblSonuc.TabIndex = 3;
+            lblSonuc.Text = "Sonuç :";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(452, 218);
+            Controls.Add(lblSonuc);
+            Controls.Add(btnKontrol);
+            Controls.Add(cbAlfabe);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cbAlfabe;
+        private Button btnKontrol;
+        private Label lblSonuc;
+    }
+}
---
+++ alfabe-1575/Form1.cs
@@ -0,0 +1,107 @@
+namespace alfabe_1575
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKontrol_Click(object sender, EventArgs e)
+        {
+            String harf = cbAlfabe.Text;
+            switch (harf)
+            {
+                case "A":
+                    lblSonuc.Text = "A Harfi alfabenin 1. harfidir.";
+                    break;
+                case "B":
+                    lblSonuc.Text = "B Harfi alfabenin 2. harfidir.";
+                    break;
+                case "C":
+                    lblSonuc.Text = "C Harfi alfabenin 3. harfidir.";
+                    break;
+                case "Ç":
+                    lblSonuc.Text = "Ç Harfi alfabenin 4. harfidir.";
+                    break;
+                case "D":
+                    lblSonuc.Text = "D Harfi alfabenin 5. harfidir.";
+                    break;
+                case "E":
+                    lblSonuc.Text = "E Harfi alfabenin 6. harfidir.";
+                    break;
+                case "F":
+                    lblSonuc.Text = "F Harfi alfabenin 7. harfidir.";
+                    break;
+                case "G":
+                    lblSonuc.Text = "G Harfi alfabenin 8. harfidir.";
+                    break;
+                case "Ð":
+                    lblSonuc.Text = "Ð Harfi alfabenin 9. harfidir.";
+                    break;
+                case "H":
+                    lblSonuc.Text = "H Harfi alfabenin 10. harfidir.";
+                    break;
+                case "I":
+                    lblSonuc.Text = "I Harfi alfabenin 11. harfidir.";
+                    break;
+                case "Ý":
+                    lblSonuc.Text = "Ý Harfi alfabenin 12. harfidir.";
+                    break;
+                case "J":
+                    lblSonuc.Text = "J Harfi alfabenin 13. harfidir.";
+                    break;
+                case "K":
+                    lblSonuc.Text = "K Harfi alfabenin 14. harfidir.";
+                    break;
+                case "L":
+                    lblSonuc.Text = "L Harfi alfabenin 15. harfidir.";
+                    break;
+                case "M":
+                    lblSonuc.Text = "M Harfi alfabenin 16. harfidir.";
+                    break;
+                case "N":
+                    lblSonuc.Text = " N Harfi alfabenin 17. harfidir.";
+                    break;
+                case "O":
+                    lblSonuc.Text = "O Harfi alfabenin 18. harfidir.";
+                    break;
+                case "Ö":
+                    lblSonuc.Text = "Ö Harfi alfabenin 19. harfidir.";
+                    break;
+                case "P":
+                    lblSonuc.Text = "P Harfi alfabenin 20. harfidir.";
+                    break;
+                case "R":
+                    lblSonuc.Text = "R Harfi alfabenin 21. harfidir.";
+                    break;
+                case "S":
+                    lblSonuc.Text = " S Harfi alfabenin 22. harfidir.";
+                    break;
+                case "Þ":
+                    lblSonuc.Text = "Þ Harfi alfabenin 23. harfidir.";
+                    break;
+                case "T":
+                    lblSonuc.Text = "T Harfi alfabenin 24. harfidir.";
+                    break;
+                case "U":
+                    lblSonuc.Text = "U Harfi alfabenin 25. harfidir.";
+                    break;
+                case "Ü":
+                    lblSonuc.Text = " Ü Harfi alfabenin 26. harfidir.";
+                    break;
+                case "V":
+                    lblSonuc.Text = "V Harfi alfabenin 27. harfidir.";
+                    break;
+                case "Y":
+                    lblSonuc.Text = "Y Harfi alfabenin 28. harfidir.";
+                    break;
+                
+
+
+
+
+            }
+        }
+    }
+}
